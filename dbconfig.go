4591ea6394ad146cede3214f345387aba51ada4b
Keenan-Faure <keenan.asterisk@gmail.com>
2023-08-28 16:57:57 +0200

    Db Conn

---
+++ dbconfig.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"integrator/internal/database"
+	"log"
+)
+
+// Initiates a connection to the database and
+// if successful returns the connection
+func InitConn(dbURL string) (DbConfig, error) {
+	db, err := sql.Open("mysql", dbURL)
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+		return DbConfig{}, err
+	}
+	return storeConfig(db), nil
+}
+
+// Stores the database connection inside a config struct
+func storeConfig(conn *sql.DB) DbConfig {
+	_, err := database.New(conn).GetUserByName(context.Background(), "abc123")
+	log.Println(err)
+	if err == nil {
+		config := DbConfig{
+			DB:    database.New(conn),
+			Valid: true,
+		}
+		return config
+	} else {
+		config := DbConfig{
+			DB:    database.New(conn),
+			Valid: false,
+		}
+		return config
+	}
+}
