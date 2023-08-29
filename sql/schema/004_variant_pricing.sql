3d2135117faf914945afe6f3767b906943dbc33c
keenan-faure <keenan.asterisk@gmail.com>
2023-08-29 21:41:32 +0200

    renamed & added new table

--- sql/schema/004_variant_pricing.sql
+++ sql/schema/004_variant_pricing.sql
@@ -1,5 +1,6 @@
-CREATE TABLE product_pricing(
+CREATE TABLE variant_pricing(
     id VARCHAR(32) UNIQUE NOT NULL,
+    variant_id VARCHAR(32) UNIQUE NOT NULL,
     name VARCHAR(16) NOT NULL,
     value VARCHAR(32) NOT NULL,
     created_at TIMESTAMP NOT NULL,
---
+++ sql/schema/005_variant_qty.sql
@@ -0,0 +1,8 @@
+CREATE TABLE variant_qty(
+    id VARCHAR(32) UNIQUE NOT NULL,
+    variant_id VARCHAR(32) UNIQUE NOT NULL,
+    name VARCHAR(16) NOT NULL,
+    value VARCHAR(32) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+)
