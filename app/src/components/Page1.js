a700b7bf47e1884ea6b73cb4204834c9f2c7b2c3
MrKkyle <Kyle.apocrypha@gmail.com>
2023-09-07 11:53:20 +0200

    updated

--- app/src/components/Page1.js
+++ app/src/components/Page1.js
@@ -27,9 +27,44 @@ function Page1(props)
             for(let i = 0; i < pan.length; i ++)
             {
                 pan[i].style.display = "block";
-                pan[i].style.display = "appear 1s ease-in";
+                pan[i].style.animation = "appear 1.2s ease-in";
             }
         }, 1400);
+
+        /* filter element animation */
+        let filters = document.querySelector(".filter").children;
+        setTimeout(() =>
+        {
+            for(let i = 0; i < filters.length; i ++)
+            {
+                filters[i].style.display = "block";
+                filters[i].style.animation = "appear 1.2s ease-in";
+            }
+        }, 1400);
+
+        /* filter image script to show when clicked on */
+        let filter = document.querySelectorAll(".filter-elements");
+        let filter_img = document.querySelectorAll(".filter-img");
+        let C_filter = document.querySelector(".filter-button");
+        for(let i = 0; i < filter.length; i++)
+        {
+            filter[i].addEventListener("click", () =>
+            {
+                filter_img[i].style.display = "block";
+                filter[i].style.backgroundColor = "rgba(64, 165, 24, 0.7)";
+            });
+
+            C_filter.addEventListener("click", () =>
+            {
+                filter_img[i].style.display = "none";
+                filter[i].style.backgroundColor = "rgba(61, 61, 61, 0.7)";
+            });
+        }
+
+        
+        
+
+
 
 
     }, []);
@@ -40,9 +75,19 @@ function Page1(props)
         <>
             <Background />
             <div className = "filter">
+                <div className = "filter-title"><b>Available Filters:</b></div>
+                <br /><br />
+                <div className = "filter-elements">Filter 1<div className = "filter-img"></div></div>
+                <div className = "filter-elements">Filter 2<div className = "filter-img"></div></div>
+                <div className = "filter-elements">Filter 3<div className = "filter-img"></div></div>
+                <div className = "filter-elements">Filter 4<div className = "filter-img"></div></div>
+                <br />
 
+                <button className = "filter-button">Clear Filter</button>
             </div>
             <div className = "main">
+                <div className = "search">
+                </div>
                 <div className = "main-elements">
                     <div className = "pan"></div>
                     <div className = "pan"></div>
