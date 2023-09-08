21d46b6af909cfe1a8a516888cd5a03e39f77e30
MrKkyle <Kyle.apocrypha@gmail.com>
2023-09-08 11:38:20 +0200

    updated

--- app/src/JS/Customers.js
+++ app/src/JS/Customers.js
@@ -1,13 +1,32 @@
-
+import {useEffect} from 'react';
 import Page1 from '../components/Page1';
 import '../CSS/page1.css';
 
 /* Must start with a Caps letter */
 function Customers(props)
 {
+    useEffect(()=> 
+    {
+        /* Ensures the navbar is set correctly */
+        let navigation = document.getElementById("navbar");
+        let model = document.getElementById("model");
+        let main = document.querySelector(".main");
+        window.onload = function(event)
+        {
+            navigation.style.left = "0%";
+            navigation.style.position = "relative";
+            navigation.style.width = "100%";
+            model.style.animation = "none";
+            main.style.animation = "SlideUp3 1.2s ease-in";
+        }
+        
+    }, []);
+
     return (
         <>
-            <Page1 />
+            <Page1 
+            filter_display = "none" main_bgc = "transparent" main_top = "53%" main_left = "50%" transform = "translate(-50%, -50%)"
+            width = "70%" height = "90%" animation = "SlideUp3 1.2s ease-in"/>
             
         </>
     );
