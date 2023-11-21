32ee3a0e3ba4b5731e8517b13bd342f77edca980
Keenan-Faure <keenan.asterisk@gmail.com>
2023-11-21 13:22:04 +0200

    update

--- scripts/migrations.sh
+++ scripts/migrations.sh
@@ -2,29 +2,16 @@
 
 echo "---running migrations on server container---"
 
+cd /keenan/sql/schema
+source .env
+
+echo "Checking GOOSE version"
+goose -version
+
 SSL_MODE="?sslmode=disable"
 DRIVER="postgres://"
 
-if [ ! -z "$1" ]; then
-    if [ $1 == "ci" ]; then
-        cd ./sql/schema
-        echo "Checking GOOSE version"
-        goose -version
-        DB_STRING="${DRIVER}${DB_USER}${DB_PSW}@localhost:5432/${DB_NAME}${SSL_MODE}"
-        echo "running migrations on '${DB_NAME}'"
-        goose postgres "$DB_STRING" up
-    else
-        echo "invalid parameter passed, expected 'ci'"
-        exit;
-    fi
-    # postgres://postgres:postgres@postgres:5432/
-else
-    cd /keenan/sql/schema
-    source .env
-
-    echo "Checking GOOSE version"
-    goose -version
-    DB_STRING="${DRIVER}${DB_USER}${DB_PSW}@postgres:5432/${DB_NAME}${SSL_MODE}"
-    echo "running migrations on '${DB_NAME}'"
-    goose postgres "$DB_STRING" up
-fi
+DB_STRING="${DRIVER}${DB_USER}:${DB_PSW}@postgres:5432/${DB_NAME}${SSL_MODE}"
+echo ${DB_STRING}
+echo "running migrations on '${DB_NAME}'"
+goose postgres "$DB_STRING" up
