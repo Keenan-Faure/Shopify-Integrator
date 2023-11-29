7c9c14b26d272bee7202728fc82b1448c2ffaca7
Keenan-Faure <keenan@stock2shop.com>
2023-11-29 16:07:28 +0200

    added migrations to automatically add settings

---
+++ sql/schema/028_app_settings.sql
@@ -0,0 +1,89 @@
+-- +goose Up
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+INSERT INTO app_settings(
+    id,
+    key,
+    description,
+    value,
+    created_at,
+    updated_at
+) VALUES (
+    uuid_generate_v4(),
+    'app_enable_shopify_fetch',
+    'Enables the automatic pulling of products from Shopify.',
+    'false',
+    NOW(),
+    NOW()
+), (
+    uuid_generate_v4(),
+    'app_enable_queue_worker',
+    'Enables the queue worker to process queue items.',
+    'false',
+    NOW(),
+    NOW()
+), (
+    uuid_generate_v4(),
+    'app_shopify_fetch_time',
+    'Duration between each product fetch from Shopify.',
+    'false',
+    NOW(),
+    NOW()
+), (
+    uuid_generate_v4(),
+    'app_enable_shopify_push',
+    'Enables products to be pushed to Shopify.',
+    'false',
+    NOW(),
+    NOW()
+), (
+    uuid_generate_v4(),
+    'app_queue_size',
+    'Maximum amount of queue items that can exist in the queue at any time.',
+    '100',
+    NOW(),
+    NOW()
+), (
+    uuid_generate_v4(),
+    'app_queue_process_limit',
+    'Maximum amount of queue items that can be processed each iteration.',
+    '10',
+    NOW(),
+    NOW()
+), (
+    uuid_generate_v4(),
+    'app_queue_cron_time',
+    'Interval between each run of the queue worker.',
+    '7',
+    NOW(),
+    NOW()
+), (
+    uuid_generate_v4(),
+    'app_fetch_add_products',
+    'Enables the creation of products that does not exist locally when fetching data from Shopify.',
+    'false',
+    NOW(),
+    NOW()
+), (
+    uuid_generate_v4(),
+    'app_fetch_overwrite_products',
+    'Enables local data to be overwritten by Shopify data if the product exists locally.',
+    'false',
+    NOW(),
+    NOW()
+), (
+    uuid_generate_v4(),
+    'app_fetch_create_price_tier_enabled',
+    'Enables price tiers to be created when fetching data from Shopify.',
+    'false',
+    NOW(),
+    NOW()
+), (
+    uuid_generate_v4(),
+    'app_fetch_sync_images',
+    'Enabled products to be pulled from Shopify when fetching data.',
+    'false',
+    NOW(),
+    NOW()
+);
