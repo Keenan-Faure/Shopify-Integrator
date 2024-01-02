ab09b5f38610dd7b7590b6c09bf59d9cb0c58e24
Keenan-Faure <keenan@stock2shop.com>
2024-01-02 11:14:21 +0200

    fixed issue with `{"errors":{"product":"Required parameter missing or invalid"}}`

--- internal/shopify/shopify.go
+++ internal/shopify/shopify.go
@@ -585,12 +585,6 @@ func (shopifyConfig *ConfigShopify) FetchHelper(endpoint, method string, body io
 	httpClient := http.Client{
 		Timeout: time.Second * 20,
 	}
-	fmt.Println(shopifyConfig.Url + "/" + endpoint)
-	fmt.Println(method)
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(body)
-	s := buf.String()
-	fmt.Println(s)
 	req, err := http.NewRequest(method, shopifyConfig.Url+"/"+endpoint, body)
 	if err != nil {
 		return &http.Response{}, err
