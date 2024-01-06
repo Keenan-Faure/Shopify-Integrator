275d8673bcd7ce775bd6ed2bc724973de9860fca
Keenan-Faure <keenan@stock2shop.com>
2024-01-06 12:42:23 +0200

    updated dockerfile & migrations.sh

--- scripts/migrations.sh
+++ scripts/migrations.sh
@@ -5,15 +5,10 @@ echo "---running migrations on server container---"
 SSL_MODE="?sslmode=disable"
 DRIVER="postgres://"
 
-cd /keenan
-
 cd sql/schema
 
 echo "Checking GOOSE version"
 goose -version
 DB_STRING="${DRIVER}${DB_USER}:${DB_PSW}@postgres:5432/${DB_NAME}${SSL_MODE}"
-echo "running migrations on '${DB_NAME}'"
-
 echo "$DB_STRING"
-
 goose postgres "$DB_STRING" up
