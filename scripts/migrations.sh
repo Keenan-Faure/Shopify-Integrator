94fdbfee832d846fbad315d94acbd2f32cd0600c
Keenan-Faure <keenan@stock2shop.com>
2024-01-06 12:46:32 +0200

    update

--- scripts/migrations.sh
+++ scripts/migrations.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 echo "---running migrations on server container---"
+echo "Checking GOOSE version"
+goose -version
 
 SSL_MODE="?sslmode=disable"
 DRIVER="postgres://"
 
+cd /keenan
+source .env
 cd sql/schema
 
-echo "Checking GOOSE version"
-goose -version
 DB_STRING="${DRIVER}${DB_USER}:${DB_PSW}@postgres:5432/${DB_NAME}${SSL_MODE}"
 echo "$DB_STRING"
 goose postgres "$DB_STRING" up
