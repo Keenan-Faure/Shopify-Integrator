c7e050a6f467edb49c6f560fa47280ab40e6f6f5
Keenan-Faure <keenan@stock2shop.com>
2024-01-08 17:10:39 +0200

    updated scripts, made them more readable

--- scripts/migrations.sh
+++ scripts/migrations.sh
@@ -1,14 +1,49 @@
 #!/bin/bash
+# Note that this is only run within the scope of the `install.sh`` script
+# if you wish to update your database to the latest version (migrations)
+# please use `update.sh``
 
-echo "---running migrations on server container---"
+function migrations() {
+    echo "---Running database migrations---"
 
-SSL_MODE="?sslmode=disable"
-DRIVER="postgres://"
+    SSL_MODE="?sslmode=disable"
+    DRIVER="postgres://"
+    if [[ "$1" == "production" ]]; then
+        cd /keenan/sql/schema
+    elif [[ "$1" == "development" ]]; then
+        cd sql/schema
+    else
+        err_msg
+    fi
 
-cd /keenan/sql/schema
+    echo "Checking GOOSE version"
+    goose -version
 
-echo "Checking GOOSE version"
-goose -version
-DB_STRING="${DRIVER}${DB_USER}:${DB_PSW}@postgres:5432/${DB_NAME}${SSL_MODE}"
-echo "running migrations on '${DB_NAME}'"
-goose postgres "$DB_STRING" up
+   if [[ "$1" == "development" ]]; then
+        DB_STRING="${DRIVER}${DB_USER}:${DB_PSW}@localhost:5432/${DB_NAME}${SSL_MODE}"
+    elif [[ "$1" == "production" ]]; then
+        DB_STRING="${DRIVER}${DB_USER}:${DB_PSW}@postgres:5432/${DB_NAME}${SSL_MODE}"
+    else
+        err_msg
+    fi
+
+    goose postgres "$DB_STRING" "$2"
+}
+
+function err_msg() {
+    echo "invalid argument"
+    echo "either enter:"
+    echo "./scripts/migrations.sh development  -  to update your development database"
+    echo "./scripts/migrations.sh production  -  to update your production (docker) database"
+    exit 1;
+}
+
+if [[ ! $# -eq 0 ]] ; then
+    if [[ "$1" == "development" || "$1" == "production" ]]; then
+        migrations "$1" "$2"
+    else
+        err_msg
+    fi
+else
+    err_msg
+fi
--- scripts/reset.sh
+++ scripts/reset.sh
@@ -4,46 +4,47 @@
 # If you are unable to run this file then run
 # chmod +x ./scripts/reset.sh
 
-echo "---reset containers---"
-
 source .env
+list="$APP_CONTAINER_NAME $SERVER_CONTAINER_NAME $DOCS_CONTAINER_NAME $DB_NAME $NGROK_CONTAINER_NAME"
 
-docker stop $APP_CONTAINER_NAME
-docker stop $SERVER_CONTAINER_NAME
-docker stop $DOCS_CONTAINER_NAME
-docker stop $DB_NAME
-docker stop $NGROK_CONTAINER_NAME
+function docker_stop() {
+  echo "--stopping active containers--"
+  for item in $list
+  do
+    docker stop $item
+  done
+}
 
-echo "---"
-echo "removing containers"
+function docker_rm() {
+  echo "--removing containers--"
+  for item in $list
+  do
+    docker rm $item
+  done
+}
 
-docker rm $SERVER_CONTAINER_NAME
-docker rm $APP_CONTAINER_NAME
-docker rm $DOCS_CONTAINER_NAME
-docker rm $DB_NAME
-docker rm $NGROK_CONTAINER_NAME
+function docker_rmi() {
+  echo "--removing container images--"
+  for item in $list
+  do
+    if docker image inspect $item >/dev/null 2>&1; then
+      docker rmi $(docker images $item -a -q) -f
+    else
+      echo "'$item' does not exist."
+    fi
+  done
+}
 
-#removes images
+docker_stop
+docker_rm
 
-echo "---"
-echo "removing images containers"
-
-if docker image inspect $IMAGE_NAME >/dev/null 2>&1; then
-  docker rmi $(docker images $IMAGE_NAME -a -q) -f
-else
-  echo "'$IMAGE_NAME' does not exist."
+if [[ ! $# -eq 0 ]] ; then
+    if [[ "$1" == "rmi" ]]; then
+      docker_rmi
+    fi
 fi
+# docker_rmi
 
-if docker image inspect $APP_IMAGE_NAME >/dev/null 2>&1; then
-  docker rmi $(docker images $APP_IMAGE_NAME -a -q) -f
-else
-  echo "'$APP_IMAGE_NAME' does not exist."
-fi
-
-if docker image inspect $DOCS_IMAGE_NAME >/dev/null 2>&1; then
-  docker rmi $(docker images $DOCS_IMAGE_NAME -a -q) -f
-else
-  echo "'$DOCS_IMAGE_NAME' does not exist."
-fi
-
-echo "Please re-run './scripts/install.sh'"
+echo "+-------------------------------------+"
+echo "|Please re-run './scripts/install.sh' |"
+echo "+-------------------------------------+"
