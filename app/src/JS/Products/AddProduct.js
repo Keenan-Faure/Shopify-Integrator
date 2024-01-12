36fc23d812b220cf4276b83c75c8ec39d41947f7
MrKkyle <Kyle.apocrypha@gmail.com>
2024-01-12 14:38:10 +0200

    updated- broken import and bad nav bar zIndex

--- app/src/JS/Products/AddProduct.js
+++ app/src/JS/Products/AddProduct.js
@@ -73,7 +73,7 @@ function Add_Product()
 
         console.log(Object);
 
-        /*
+        
         const api_key = localStorage.getItem('api_key');
         
         $.ajaxSetup({ headers: { 'Authorization': 'ApiKey ' + api_key} });
@@ -86,7 +86,7 @@ function Add_Product()
         {
             alert(xhr.responseText);
         });
-        */
+        
     }
 
     useEffect(() =>
