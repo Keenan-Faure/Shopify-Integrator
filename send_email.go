6ed062eb7d3d114ba99613d14da8408d07cc5cad
keenan-faure <keenan.asterisk@gmail.com>
2024-01-16 11:03:50 +0200

    updated view of emails - removed the bold

--- send_email.go
+++ send_email.go
@@ -12,8 +12,8 @@ func SendEmail(token uuid.UUID, email, name string) error {
 	m := mail.NewMessage()
 	m.SetHeader("From", utils.LoadEnv("email"))
 	m.SetHeader("To", email)
-	m.SetHeader("Subject", "Shopify-Integrator Authentication Token")
-	m.SetBody("text/html", fmt.Sprintf("<h4>Hi <b>%s</b>,</h4> <br/> Token: <br/> <textarea style='resize:none' rows='2' cols='40'>%v</textarea>", name, token.String()))
+	m.SetHeader("Subject", "Shopify Integrator Registration")
+	m.SetBody("text/html", fmt.Sprintf("Hi %s, <br />p>Please find your registration token below.</p>Token: <br><textarea style='resize:none' rows='2' cols='40'>%v</textarea>", name, token.String()))
 	d := mail.NewDialer("smtp.gmail.com", 587, utils.LoadEnv("email"), utils.LoadEnv("email_psw"))
 
 	if err := d.DialAndSend(m); err != nil {
