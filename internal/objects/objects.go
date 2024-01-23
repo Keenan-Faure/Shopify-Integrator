39855257fbf62a7a751ec5c98b0b96af559ff557
keenan-faure <keenan.asterisk@gmail.com>
2024-01-23 15:03:05 +0200

    added ngrok response

--- internal/objects/objects.go
+++ internal/objects/objects.go
@@ -6,6 +6,45 @@ import (
 	"github.com/google/uuid"
 )
 
+// ngrok.go
+type NgrokTunnelResponse struct {
+	Tunnels []struct {
+		Name      string `json:"name"`
+		ID        string `json:"ID"`
+		URI       string `json:"uri"`
+		PublicURL string `json:"public_url"`
+		Proto     string `json:"proto"`
+		Config    struct {
+			Addr    string `json:"addr"`
+			Inspect bool   `json:"inspect"`
+		} `json:"config"`
+		Metrics struct {
+			Conns struct {
+				Count  int `json:"count"`
+				Gauge  int `json:"gauge"`
+				Rate1  int `json:"rate1"`
+				Rate5  int `json:"rate5"`
+				Rate15 int `json:"rate15"`
+				P50    int `json:"p50"`
+				P90    int `json:"p90"`
+				P95    int `json:"p95"`
+				P99    int `json:"p99"`
+			} `json:"conns"`
+			HTTP struct {
+				Count  int `json:"count"`
+				Rate1  int `json:"rate1"`
+				Rate5  int `json:"rate5"`
+				Rate15 int `json:"rate15"`
+				P50    int `json:"p50"`
+				P90    int `json:"p90"`
+				P95    int `json:"p95"`
+				P99    int `json:"p99"`
+			} `json:"http"`
+		} `json:"metrics"`
+	} `json:"tunnels"`
+	URI string `json:"uri"`
+}
+
 // shopify.go
 type ShopifyWebhookRequest struct {
 	ShopifyWebhook `json:"webhook,omitempty"`
