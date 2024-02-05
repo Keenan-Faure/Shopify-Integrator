198e0ad957f36fc803853bf4d77e70e184ce7f19
Keenan-Faure <keenan@stock2shop.com>
2024-02-05 15:59:50 +0200

    implemented the ProductIDhandle with BasicAuth

--- httprouter_gin.go
+++ httprouter_gin.go
@@ -1,8 +1,51 @@
 package main
 
 import (
+	"errors"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
+
+// TIPS
+// URLParams | name := c.Param("name")
+// Query Params | c.Query("lastname")
+
+/*
+Returns the product data having the specific id
+
+Authorization: Basic, QueryParams, Headers
+
+Response-Type: application/json
+
+HTTP Codes: 200, 503
+*/
+func (dbconfig *DbConfig) ProductIDHandle() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		product_id := c.Param("id")
+		err := IDValidation(product_id)
+		if err != nil {
+			RespondWithError(c, err, http.StatusInternalServerError)
+			return
+		}
+		product_uuid, err := uuid.Parse(product_id)
+		if err != nil {
+			RespondWithError(c, errors.New("could not decode product id: "+product_id), http.StatusBadRequest)
+			return
+		}
+		product_data, err := CompileProductData(dbconfig, product_uuid, c.Request.Context(), false)
+		if err != nil {
+			if err.Error() == "sql: no rows in result set" {
+				RespondWithError(c, errors.New("not found"), http.StatusNotFound)
+				return
+			}
+			RespondWithError(c, err, http.StatusInternalServerError)
+			return
+		}
+		RespondWithJSON(c, product_data, http.StatusOK)
+	}
+}
 
 /*
 Confirms if the API is ready to start accepting requests.
@@ -13,12 +56,23 @@ Response-Type: application/json
 
 HTTP Codes: 200, 503
 */
-func (dbconfig *DbConfig) ReadyHandle() {
-	r := gin.Default()
-	r.GET("/ready", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "OK",
-		})
+func (dbconfig *DbConfig) ReadyHandle() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if dbconfig.Valid {
+			RespondWithJSON(c, gin.H{"message": "OK"}, http.StatusOK)
+		} else {
+			RespondWithError(c, errors.New("Unavailable"), http.StatusServiceUnavailable)
+		}
+	}
+}
+
+func RespondWithError(c *gin.Context, err error, http_code int) {
+	c.Error(err)
+	c.AbortWithStatusJSON(http_code, gin.H{
+		"message": err.Error(),
 	})
-	r.Run()
+}
+
+func RespondWithJSON(c *gin.Context, payload any, http_code int) {
+	c.JSON(http_code, payload)
 }
