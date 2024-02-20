3812287f911fa9b67cabb3fb2df9e8ee83490cee
Keenan-Faure <keenan@stock2shop.com>
2024-02-20 15:50:44 +0200

    created new file to store database interactions

---
+++ database_helper.go
@@ -0,0 +1,7 @@
+package main
+
+/*
+This file contains utility functions that attempts to shorten the amount of lines
+And keep the code DRY used in the application.
+Functions are mostly used to interact with the database.
+*/
