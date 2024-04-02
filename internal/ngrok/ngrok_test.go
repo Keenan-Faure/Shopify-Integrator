2eedb1237f9bb4aa1b97adf7864bf96b2d1d13a8
Keenan-Faure <keenan@stock2shop.com>
2024-04-02 16:58:35 +0200

    added ngrok test function TestFetchNgrokTunnels()

--- internal/ngrok/ngrok_test.go
+++ internal/ngrok/ngrok_test.go
@@ -1,70 +1,94 @@
 package ngrok
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"objects"
+	"os"
 	"testing"
 
+	"github.com/go-playground/assert/v2"
 	"github.com/jarcoal/httpmock"
 )
 
-const NGROK_URL = "http://localhost:8888"
+const NGROK_URL = "http://ngrok.api.localhost:8888"
 
 func TestFetchNgrokTunnels(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	response := objects.NgrokTunnelResponse{
-		Tunnels: []struct {
-			Name      string "json:\"name\""
-			ID        string "json:\"ID\""
-			URI       string "json:\"uri\""
-			PublicURL string "json:\"public_url\""
-			Proto     string "json:\"proto\""
-			Config    struct {
-				Addr    string "json:\"addr\""
-				Inspect bool   "json:\"inspect\""
-			} "json:\"config\""
-			Metrics struct {
-				Conns struct {
-					Count  int "json:\"count\""
-					Gauge  int "json:\"gauge\""
-					Rate1  int "json:\"rate1\""
-					Rate5  int "json:\"rate5\""
-					Rate15 int "json:\"rate15\""
-					P50    int "json:\"p50\""
-					P90    int "json:\"p90\""
-					P95    int "json:\"p95\""
-					P99    int "json:\"p99\""
-				} "json:\"conns\""
-				HTTP struct {
-					Count  int "json:\"count\""
-					Rate1  int "json:\"rate1\""
-					Rate5  int "json:\"rate5\""
-					Rate15 int "json:\"rate15\""
-					P50    int "json:\"p50\""
-					P90    int "json:\"p90\""
-					P95    int "json:\"p95\""
-					P99    int "json:\"p99\""
-				} "json:\"http\""
-			} "json:\"metrics\""
-		}{},
-		URI: "",
-	}
-
-	// Exact URL match
-	httpmock.RegisterResponder("GET", NGROK_URL+"api/tunnels",
+	response := CreateTestNgrokPayload("test-case-valid-data.json")
+	httpmock.RegisterResponder("GET", NGROK_URL+"/api/tunnels",
 		func(req *http.Request) (*http.Response, error) {
 			resp, err := httpmock.NewJsonResponse(200, response)
 			if err != nil {
 				return httpmock.NewStringResponse(500, ""), nil
 			}
 			return resp, nil
-		})
+		},
+	)
+
+	res, err := fetchHelper(NGROK_URL, "api/tunnels", http.MethodGet, nil)
+	if err != nil {
+		t.Errorf("expected 'nil' but found: " + err.Error())
+	}
+	defer res.Body.Close()
+	respBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected 'nil' but found: " + err.Error())
+	}
+	if res.StatusCode != 200 {
+		t.Errorf("expected '200' but found: " + fmt.Sprint(res.StatusCode))
+	}
+	tunnels := objects.NgrokTunnelResponse{}
+	err = json.Unmarshal(respBody, &tunnels)
+	if err != nil {
+		t.Errorf("expected 'nil' but found: " + err.Error())
+	}
 
 	// get count info
 	httpmock.GetTotalCallCount()
-	info := httpmock.GetCallCountInfo()
-	fmt.Println(info)
+	requestInfo := httpmock.GetCallCountInfo()
+
+	// Test 1 - Assert if the amount of API calls that is made is correct.
+	assert.Equal(t, 1, requestInfo["GET "+NGROK_URL+"/api/tunnels"])
+
+	// Test 2 - Assert if the response that we receive is correct and is the correct body
+	assert.Equal(t, 1, len(response.Tunnels))
+	assert.Equal(t, "website", response.Tunnels[0].Name)
+	assert.Equal(t, "/api/tunnels/website", response.Tunnels[0].URI)
+	assert.Equal(t, "http://host.docker.internal:8080", response.Tunnels[0].Config.Addr)
+	assert.Equal(t, "/api/tunnels", response.URI)
+}
+
+/* Returns a test user RequestBodyRegister struct */
+func CreateTestNgrokPayload(fileName string) objects.NgrokTunnelResponse {
+	fileBytes := payload("./test_payloads/" + fileName)
+	ngrokTunnelResponse := objects.NgrokTunnelResponse{}
+	err := json.Unmarshal(fileBytes, &ngrokTunnelResponse)
+	if err != nil {
+		log.Println(err)
+	}
+	return ngrokTunnelResponse
+}
+
+/*
+Returns a byte array representing the file data that was read
+
+Data is retrived from the project directory `test_payloads`
+*/
+func payload(filePath string) []byte {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+	respBody, err := io.ReadAll(file)
+	if err != nil {
+		log.Println(err)
+	}
+	return respBody
 }
