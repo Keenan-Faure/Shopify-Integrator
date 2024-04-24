859d8468006316777db87ec7b147ed2e1fcf3d12
Keenan-Faure <keenan@stock2shop.com>
2024-04-24 13:54:41 +0200

    added function that create custom database connection to postgres for custom queries

--- config.go
+++ config.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"database/sql"
+	"flag"
 	"integrator/internal/database"
 	"log"
+	"utils"
+
+	"github.com/jackc/pgx/v5"
 )
 
 // Initiates a connection to the database and
@@ -12,10 +16,37 @@ import (
 func InitConn(dbURL string) (DbConfig, error) {
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
-		log.Fatal("Error connecting to the database: ", err)
+		log.Fatal("Unable to connect to database: ", err)
 		return DbConfig{}, err
 	}
 	return storeConfig(db), nil
+}
+
+// Initiates a connection to the database
+// used only for custom queries that do not
+// exist inside the /internal/database
+func InitCustomConnection(dbURL string) *pgx.Conn {
+	conn, err := pgx.Connect(context.Background(), dbURL)
+	if err != nil {
+		log.Fatal("Unable to connect to database: ", err)
+	}
+	return conn
+}
+
+// Creates a connection string
+func InitConnectionString(mock bool) string {
+	use_localhost := flag.Bool("localhost", false, "Enable localhost for tests only")
+	flag.Parse()
+
+	connection_string := "postgres://" + utils.LoadEnv("db_user") + ":" + utils.LoadEnv("db_psw")
+	host := "@localhost:5432/"
+	if !*use_localhost {
+		host = "@postgres:5432/"
+	}
+	if mock {
+		host = "@localhost:5432/"
+	}
+	return connection_string + host + utils.LoadEnv("db_name") + "?sslmode=disable"
 }
 
 // Stores the database connection inside a config struct
