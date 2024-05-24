3766168358fdffda39f280a184470f6577e2fdec
Keenan-Faure <keenan@stock2shop.com>
2024-05-24 16:55:40 +0200

    changed the host names

--- middleware.go
+++ middleware.go
@@ -10,7 +10,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", utils.LoadEnv("APP_HOST"))
+		c.Writer.Header().Set("Access-Control-Allow-Origin", utils.LoadEnv("CORS_APP_HOST"))
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
